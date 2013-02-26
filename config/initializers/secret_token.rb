d912160aab552cad125a2decc4dad836ec7ad235
Alex <alex@ubuntu.(none)>
2013-02-26 17:24:37 +0100

    intialized app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kokko::Application.config.secret_token = '5ad8bd275e21d1808624572fd0a728de533e492188a57cd0c45e8aa8543ba52e923ea9344eafea3a1ecde3711323ed7485866019c2b20b208e78fd20aacab430'
